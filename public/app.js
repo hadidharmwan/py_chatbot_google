62bf53899c17abd356f64d0932491d06c518dcba
hadidharmawan <hadidharmawan32@gmail.com>
2021-03-21 15:38:59 +0700

    add function userSubmitEventHandler

--- public/app.js
+++ public/app.js
@@ -1,3 +1,15 @@
 const  submitButton = document.getElementById('submitButton');
 const chatbotInput = document.getElementById('chatbotInput');
 const chatbotOutput = document.getElementById('chatbotOutput');
+
+submitButton.onclick = userSubmitEventHandler;
+chatbotInput.onkeyup = userSubmitEventHandler;
+
+function userSubmitEventHandler(event) {
+    if(
+        (event.keyCode && event.keyCode === 13) || 
+        event.type === 'click'
+    ){
+        chatbotOutput.innerText = 'thinking...';
+    }
+}
